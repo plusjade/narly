85c21ad51556d69bd4f4f710be0316b203ed48af
Jade Dominguez <plusjade@gmail.com>
2011-10-26 23:47:24 -0500

    fresh rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superwatch::Application.config.secret_token = '95662c1be2c394ada5b02f0b61e608ae9386483047d8939828713aad9796d87ade1d446c7995347ac4bb753f3e570ca878db94b4c3f830bb40385233d8d558c2'
