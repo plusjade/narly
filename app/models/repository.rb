9a7e8135e6019f358c4831cedc9c3ad6f315b62a
Jade Dominguez <plusjade@gmail.com>
2011-10-29 16:49:24 -0500

    remove old methods that use hubruby

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,6 @@
 class Repository
   include DataMapper::Resource
-  include HubWire::User
+  include HubWire::Repo
   
   property :id, Serial
   property :ghid, Integer, :unique => true
@@ -16,23 +16,5 @@ class Repository
   
   belongs_to :user, :child_key => [:user_ghid]
 
-  #GitHub::Repository
-  def self.create_with_api(gh_repo)
-    if repo = self.find_by_uid(gh_repo.id.to_i)
-      return repo
-    else
-      create! do |repo|
-        repo.uid = gh_repo.id
-        repo.user_uid = gh_repo.owner.login["id"]
-        repo.name = gh_repo.name
-        repo.description = gh_repo.description
-        repo.language = gh_repo.language
-        repo.watchers = gh_repo.watchers
-        #repo.forks = gh_repo.forks
-      end
-    end
-    
-    
-  end
   
 end
