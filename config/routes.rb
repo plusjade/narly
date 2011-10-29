1c2bad500afc36f21615d9c9e149723363431484
Jade Dominguez <plusjade@gmail.com>
2011-10-28 20:48:32 -0500

    namespace user page with '/users/'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Superwatch::Application.routes.draw do
   root  :to => "home#index"
 
-  get "/:username" => "home#index"
+  get "/user/:username" => "home#index", :as => :user
   
   match "auth/github/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
