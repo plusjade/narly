5ef1cd505ea101b7ff5c163683771ca5cd42a11f
Jade Dominguez <plusjade@gmail.com>
2011-10-29 21:03:46 -0500

    merge Repo.rb into Repository.rb DM model and add some more data fields

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -5,16 +5,43 @@ class Repository
   property :id, Serial
   property :ghid, Integer, :unique => true
   property :user_ghid, Integer, :index => true
+  property :login, String
   property :name, String
   property :description, Text, :lazy => false
   property :language, String
   property :watchers, Integer
   property :forks, Integer
+  property :fork, Boolean
 
   property :created_at, DateTime
   property :updated_at, DateTime
   
-  belongs_to :user, :child_key => [:user_ghid]
+  belongs_to :owner, :model => User, 
+    :parent_key => [:ghid],
+    :child_key => [:user_ghid]
 
+
+  # returns array with tag_name, score.
+  # ex: ["ruby", "1", "git", "1"] 
+  #
+  def tags(limit=nil)
+    $redis.zrevrange( 
+      self.redis_key(:tags),
+      0, 
+      (limit.to_i.nil? ? -1 : limit.to_i - 1),
+      :with_scores => true
+    )
+  end
   
+  def users
+    $redis.smembers redis_key(:users)
+  end
+  
+  def html_url
+    "http://github.com/#{self.login}/#{self.name}"
+  end
+  
+  def redis_key(scope)
+    "REPO:#{self.ghid}:#{scope}"
+  end
 end
