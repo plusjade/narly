74ac584007827ab39e64af639b491e9cd4fe84ba
Jade Dominguez <plusjade@gmail.com>
2011-10-30 15:07:13 -0500

    refactor HubWire to be more DRY and simple
    This still makes a lot of assumptions and is not the best implementation, but it gets the job done for now

--- app/models/hub_wire.rb
+++ app/models/hub_wire.rb
@@ -1,84 +1,130 @@
 module HubWire
-  
-  module Helper
+
+  def self.included(model)
+    model.extend(ClassMethods)
+    class << model; attr_accessor :github_object_type end
+  end
+    
+  module ClassMethods
+
+    # Pass the github resource "type" into githubify 
+    # This let's us know what resource your model wants to build on top of.
+    # Current resources:
+    #   User
+    #   Repo
+    #
+    def githubify(opts={})
+      opts[:type] ||= "user"
+
+      self.github_object_type = opts[:type].to_s
+    end
+    
+    
+    # Load the raw json response object from github
+    # Returns json object.
+    #
+    def load_json_from_github(login, repo_name = nil)
+      type = self.github_object_type
+      
+      if type == "user"
+        HubWire::DSL::user(login)
+      elsif type == "repo"
+        HubWire::DSL::repository(login, repo_name)
+      else
+        {}
+      end
+    end
+    
+    # Load a user from github api
+    # Returns a DataMapper object
+    #
+    def new_from_github(login, repo_name = nil)
+      new_from_github_hash(load_json_from_github(login, repo_name))
+    end
+
+
+    # Spawn a new instance from the hash returned from the github api
+    #
+    def new_from_github_hash(hash)
+      return new() if hash.blank?
+      
+      type = self.github_object_type
+      
+      if type == "user"
+        
+        instance = new({:provider => "github", :ghid => hash["id"]})
+
+      elsif type == "repo"
+
+        # Todo: a better way?
+        # Make sure we have the owner
+        if User.first(:ghid => hash["owner"]["id"]).nil?
+          User.new_from_github_hash(hash["owner"]).save
+        end
+
+        instance = new({
+          :ghid => hash["id"], 
+          :user_ghid => hash["owner"]["id"],
+          :login => hash["owner"]["login"]
+        })
+        
+        hash.delete("owner")
+      else
+        instance = new()
+      end
+      
+      hash.each do |k, v|
+        if instance.respond_to?("#{k}=")
+          instance.send("#{k}=", v)
+        end
+      end
+      
+      instance.id = nil # don't set this via the attrs
+      instance
+    end
+    
+  end # ClassMethods
+
+
+  # Return JSON from github
+  # This was taken from https://github.com/diogenes/hubruby
+  #
+  module DSL
+
+    def self.user(login)
+      json("/users/#{login}")
+    end
+
+    def self.following(login)
+      json("/users/#{login}/following")
+    end
+
+    def self.followers(login)
+      json("/users/#{login}/followers")
+    end
+
+    def self.repositories(login)
+      json("/users/#{login}/repos")
+    end
+
+    def self.watched(login)
+      json("/users/#{login}/watched")
+    end
+
+    def self.repository(login, repository_name)
+      json("/repos/#{login}/#{repository_name}")
+    end
+
+    def self.watched_by(login, repository_name)
+      json("/repos/#{login}/#{repository_name}/watchers")
+    end
 
     private
 
-    def json(path)
-      data = HTTParty.get('https://api.github.com' << path).parsed_response
-      data["message"].blank? ? data : {}
-    end
-    
-  end
-    
-  module User
-    include Helper
-    
-    def self.included(model)  
-      model.extend(self::ClassMethods)
-      model.extend(Helper)
-    end
-    
-    # get this user
-    def get_user
-      json("/users/#{self.login}")
+    def self.json(path)
+      HTTParty.get('https://api.github.com' << path).parsed_response
     end
 
-    # get repos owned by this user
-    def get_repos
-      json("/users/#{self.login}/repos")
-    end
-
-    # get watched repos from this user
-    def get_watched
-      json("/users/#{self.login}/watched")
-    end
-
-    module ClassMethods
-      
-      # Load the raw json response object from github
-      # Returns json object.
-      #
-      def load_json_from_github(login)
-        json("/users/#{login}")
-      end
-      
-      # Load a user from github api
-      # Returns a DataMapper object
-      #
-      def load_from_github(login)
-        attributes = load_json_from_github(login)
-        return new() if attributes.blank?
-        
-        user = new({:provider => "github", :ghid => attributes["id"]})
-        attributes.each do |k, v|
-          if user.respond_to?("#{k}=")
-            user.send("#{k}=", v)
-          end
-        end
-        user.id = nil # we don't want to set this via the attrs
-        user
-      end
-
-    end
-    
-    
-  end # User
-
-  module Repo
-    include Helper
-    
-    # get users watching this repo
-    def get_watched_by
-      json("/repos/#{self.owner.login}/#{self.name}/watchers")
-    end
-
-    # get a single repo 
-    def get_repo
-      json("/repos/#{self.login}/#{self.name}")
-    end
-    
-  end # Repo
+  end # DSL
   
-  
-end # Rover
+end # HubWire
