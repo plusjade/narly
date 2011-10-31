a89272760fefda2fba2a75248b0edeb2f4d012a8
Jade Dominguez <plusjade@gmail.com>
2011-10-31 07:40:29 -0500

    Hack out some basic tag formatting rules

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,8 +2,11 @@ class Tag
   
   attr_accessor :name
 
+  BlackList = /[^a-z 0-9 + # - .]/
+  
   def initialize(name)
-    self.name = name.downcase
+    self.name = name.to_s.downcase.gsub(BlackList, "")
+    raise "tag cant be blank" if self.name.blank?
   end
   
   def redis_key(scope)
@@ -16,6 +19,19 @@ class Tag
   
   def users
     $redis.smembers redis_key(:users)
+  end
+  
+  def self.new_from_tag_string(tag_string)
+    tag_string.to_s.split(":").map do |name|
+      new(name)
+    end
+  end
+  
+  # Takes one or more Tag instances (via array) and provides
+  # the equivalent tag_string
+  #
+  def self.to_tag_string(tags)
+    Array(tags).map! { |tag| tag.name }.join(":")
   end
   
 end
