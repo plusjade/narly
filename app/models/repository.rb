51ff65d2ca18d3f10df77c197def56e5b83b4350
Jade Dominguez <plusjade@gmail.com>
2011-10-31 23:32:52 -0500

    Overwrite DM finder for Repository to try load_from_github on miss
    Also require main fields so the resource will fail to save otherwise

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -7,10 +7,10 @@ class Repository
   define_tag_strategy :namespace => "REPO", :scope_by_field => :ghid
   
   property :id, Serial
-  property :ghid, Integer, :unique => true
-  property :user_ghid, Integer, :index => true
-  property :login, String
-  property :name, String
+  property :ghid, Integer, :unique => true, :required => true
+  property :user_ghid, Integer, :index => true, :required => true
+  property :login, String, :required => true
+  property :name, String, :required => true
   property :description, Text, :lazy => false
   property :language, String
   property :watchers, Integer
@@ -25,6 +25,24 @@ class Repository
     :child_key => [:user_ghid]
 
 
+  # Overwrite DM finder to try load_from_github on miss
+  # This is so we can save this repo behind the scenes.
+  #
+  def self.first(*args)
+    repo = super(*args)
+    if repo.nil? && (login = args.first[:login]) && (name = args.first[:name])
+      puts "to network!"
+      repo = new_from_github(login, name)
+      repo = nil unless repo.save
+    end  
+
+    repo
+  end
+  
+  def self.first!(*args)
+    first(*args) || raise(DataMapper::ObjectNotFoundError, "Could not find repository with conditions: #{args.first.inspect}") 
+  end
+      
   def tag_by_user(user, tag)
     add_tag_associations(user, self, tag)
   end
