ab1e3b308a5373ad3f2a53d5a89e79fb19e15d74
Jade Dominguez <plusjade@gmail.com>
2011-11-01 19:12:26 -0500

    Set up basic Repos controller logic

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,11 +1,16 @@
 class ReposController < ApplicationController
 
+  def index
+    @repos = Tag.repos(params[:tags])
+  end
+  
   def show
     @owner = User.first!(:login => params[:login].to_s)
     @repo = Repository.first!({
       :login => @owner.login, 
       :name => params[:name].to_s
     })
+    @similar_repos = @repo.similar_repos
   end
   
 end
