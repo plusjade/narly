0589c1fe7bc6a4a7c95dd3742a72bebdf882601e
Jade Dominguez <plusjade@gmail.com>
2011-11-01 19:13:24 -0500

    Change routes to more RESTful style approach
    We also need to allow periods for the repo names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Superwatch::Application.routes.draw do
   root  :to => "home#index"
 
-  get "/users/:login" => "users#show", :as => :user
-  get "/repos/:login/:name" => "repos#show", :as => :repo
+  scope "/", :constraints => {:name => /[\w\.-]+/ } do
+
+    get "/users/:login/repos/tagged/:tags" => "users#show", :as => :user_tagged
+    get "/users/:login" => "users#show", :as => :user
+  
+
+    get "/repos/tagged/:tags" => "repos#index", :as => :repos_tagged
+    get "/repos/:login/:name" => "repos#show", :as => :repo
+  
+    get "/tags" => "tags#show", :as => :tag
+    get "/tags" => "tags#index"
+  
+  end
   
   match "auth/github/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
