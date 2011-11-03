312b0a80fe06c40fe023d04c604ac311df58f4cf
Jade Dominguez <plusjade@gmail.com>
2011-11-03 02:35:22 -0500

    Redirect to user page on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.first(:provider => auth["provider"], :ghid => auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to user_path(user.login), :notice => "Signed in!"
   end
         
   def destroy
