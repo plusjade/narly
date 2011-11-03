17b0f79906bee06cc47c90e61ac0e8740f4e7ec5
Jade Dominguez <plusjade@gmail.com>
2011-11-02 20:05:40 -0500

    Add routes for getting tag data relative to repos and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Superwatch::Application.routes.draw do
   root  :to => "home#index"
 
-  scope "/", :constraints => {:name => /[\w\.-]+/ } do
+  scope "/", :constraints => {:name => /[\w\.-]+/, :ghid => /[0-9]+/ } do
 
     get "/users/:login/repos/tagged/:tags" => "users#show", :as => :user_tagged
+    get "/users/:login/repos/:ghid/tags" => "users#repo_tags", :as => :user_repo_tags
     get "/users/:login/tag" => "users#tag", :as => :user_tag
     get "/users/:login" => "users#show", :as => :user
     
-
     get "/repos/tagged/:tags" => "repos#index", :as => :repos_tagged
+    get "/repos/:ghid/tags" => "repos#tags", :as => :repo_tags
     get "/repos/:login/:name" => "repos#show", :as => :repo
   
     get "/tag" => "users#tag", :as => :tag
