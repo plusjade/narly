8caa77fbe56a44c8bbf70dd73694fd82d575bfc0
Jade Dominguez <plusjade@gmail.com>
2011-11-06 14:28:10 -0600

    Move Tag (model) specific methods back out from TagBuddy

--- app/models/tag_buddy/modules/tag_module.rb
+++ app/models/tag_buddy/modules/tag_module.rb
@@ -12,17 +12,6 @@ module TagBuddy
     end
 
     module ClassMethods
-      
-      # Spawn Tag instances from the given tag_string
-      # where tag_string is a String of the format:
-      #   tag1:tag2:tag3:tagN:...
-      # This format is used in the url as paramater
-      #
-      def new_from_tag_string(tag_string)
-        tag_string.to_s.split(":").map do |name|
-          new(:name => name)
-        end
-      end
 
       # Return an Array of items associated with the provided tags.
       # tags can be one or an Array of Tag instances or a tag_string
@@ -38,28 +27,6 @@ module TagBuddy
         TagBuddy::Query.items_by_tags(self, tags, limit)
       end
 
-      # tags_data format: 
-      #   ["ruby", "1", "git", "1"] 
-      #   where scores represent tag count and come directly after their tag.
-      #
-      def new_from_tags_data(tags_data)
-        tags = []
-        tags_data.each_with_index do |name, i|
-    			next if (i > 0 && i.odd?)
-          tags << new(:name => name, :relative_count => tags_data[i+1])
-        end
-
-        tags
-      end  
-
-      # Takes one or more Tag instances (via array) and provides
-      # the equivalent tag_string
-      #
-      def to_tag_string(tags)
-        Array(tags).map! { |tag| tag.scoped_field }.join(":")
-      end
-        
-        
     end # ClassMethods
       
     
