34531670b2c29a7abb6b6da87408cceff458c5f1
Jade Dominguez <plusjade@gmail.com>
2011-11-06 22:07:00 -0600

    Move similar_items into the Query module
    This doesn't work yet, just organizing stuf

--- app/models/tag_buddy/modules/item_module.rb
+++ app/models/tag_buddy/modules/item_module.rb
@@ -9,17 +9,6 @@ module TagBuddy
       model.namespace = "ITEM"
     end
     
-    # Return items that share this item's top 3 tags.  
-    # Ideally we want what items share the top 3 tags in *their* top n tags
-    # but that's kind of hard right now.
-    #
-    def similar_items(limit=nil)
-      keys = tags(3).map {|tag| tag.storage_key(:items) }
-      items = $redis.send(:sinter, *keys)
-      items.delete(self.items.to_s)
-      items = items[0, limit.to_i] unless limit.to_i.zero?
-    end
-    
   end # User
   
 end # TagBuddy
--- app/models/tag_buddy/modules/query_module.rb
+++ app/models/tag_buddy/modules/query_module.rb
@@ -70,6 +70,19 @@ module TagBuddy
       tag_array.sort!
     end
     
+    
+    # Return items that share this item's top 3 tags.  
+    # Ideally we want what items share the top 3 tags in *their* top n tags
+    # but that's kind of hard right now.
+    #
+    # This doesn't work
+    def self.similar_items(limit=nil)
+      keys = tags(3).map {|tag| tag.storage_key(:items) }
+      items = $redis.send(:sinter, *keys)
+      items.delete(self.items.to_s)
+      items = items[0, limit.to_i] unless limit.to_i.zero?
+    end
+    
   end # Query
   
 end # TagBuddy
