ac31d30d6003c0f119a170b49ea4f65589b6bdeb
Jade Dominguez <plusjade@gmail.com>
2011-11-06 19:07:46 -0600

    Refactory Query methods to accept conditions hash

--- app/models/tag_buddy/modules/query_module.rb
+++ app/models/tag_buddy/modules/query_module.rb
@@ -5,20 +5,22 @@ module TagBuddy
     # A collection query simply returns members from a given set
     #
     #
-    def self.collection(instance, type, limit)
-      items = $redis.smembers(instance.storage_key(type))
-      items = items[0, limit.to_i] unless limit.to_i.zero?
+    def self.collection(type, conditions)
+      items = $redis.smembers(conditions[:via].storage_key(type))
+      items = items[0, conditions[:limit].to_i] unless conditions[:limit].to_i.zero?
       items
     end
     
     # returns array with tag_name, score.
     # ex: ["ruby", "1", "git", "1"] 
     #
-    def self.tags(class_or_instance, limit=nil)
+    def self.tags(conditions)
+      scope = conditions[:users] || conditions[:items]
+      
       $redis.zrevrange( 
-        class_or_instance.storage_key(:tags),
+        scope.storage_key(:tags),
         0, 
-        (limit.to_i.nil? ? -1 : limit.to_i - 1),
+        (conditions[:limit].to_i.nil? ? -1 : conditions[:limit].to_i - 1),
         :with_scores => true
       )
     end
@@ -26,18 +28,19 @@ module TagBuddy
     # Get items tagged by this user with a particular tag or set of tags.
     # tags is a single or an array of Tag instances
     #
-    def self.items_via(class_or_instance, tags, limit = nil)
-      tags = Array(tags)
+    def self.items_via(conditions)
+      tags = Array(conditions[:tags])
+      users = Array(conditions[:users])
       
       # users have different storage_keys, how to merge?
-      if class_or_instance.is_a?(User)
-        keys = tags.map { |tag| class_or_instance.storage_key(:tag, tag.scoped_field, :items) }
+      if users.first.is_a?(User)
+        keys = tags.map { |tag| users.first.storage_key(:tag, tag.scoped_field, :items) }
       else
         keys = tags.map { |tag| tag.storage_key(:items) }
       end
         
       items = $redis.send(:sinter, *keys)
-      items = items[0, limit.to_i] unless limit.to_i.zero?
+      items = items[0, conditions[:limit].to_i] unless conditions[:limit].to_i.zero?
       items
     end
     
@@ -49,18 +52,19 @@ module TagBuddy
     #tag.storage_key(:users)
     #item.storage_key(:users)
     #
-    def self.users_via(items, tags, limit = nil)
-      keys = Array(tags).map { |tag| tag.storage_key(:users) }
-      Array(items).each { |item| keys << item.storage_key(:users) }
+    def self.users_via(conditions)
+      
+      keys = Array(conditions[:tags]).map { |tag| tag.storage_key(:users) }
+      Array(conditions[:items]).each { |item| keys << item.storage_key(:users) }
         
       users = $redis.send(:sinter, *keys)
-      users = users[0, limit.to_i] unless limit.to_i.zero?
+      users = users[0, conditions[:limit].to_i] unless conditions[:limit].to_i.zero?
       users
     end
         
   
-    def self.tags_via(user, item, limit = nil)
-      tag_array = $redis.hget user.storage_key(:items, :tags), item.scoped_field
+    def self.tags_via(conditions)
+      tag_array = $redis.hget conditions[:users].storage_key(:items, :tags), conditions[:items].scoped_field
       tag_array = tag_array ? ActiveSupport::JSON.decode(tag_array) : []
 
       tag_array.sort!
