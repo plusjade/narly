1e74bef42a1a2b25130d30febf652b307c0c02bf
Jade Dominguez <plusjade@gmail.com>
2011-11-12 17:12:16 -0600

    Track my noob attempt at Backbone.js
    This took me forever to do because I was reading docs for 0.5.3 and following a tutorial that loaded 0.3.3. Just goes to show tutorials should track package versions and also - I should be writing tutorials jackass. This implementation actually is working finally but is in no way proper backbone so lets get to it

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,6 +1,154 @@
 
 $(function(){
 	var currentUserLogin = $("#current_user_data").text();
+// MODELS
+	var Repo = Backbone.Model.extend({
+		defaults : {
+			"full_name" : "plusjade/plusjade",
+			"name" : "name",
+			"login" : "login",
+		},
+		initialize : function(){
+			console.log("repo initialized");
+			
+			this.tags = new TagCollection;
+	    this.tags.url = '/repos/' + this.get("full_name") + '/tags';
+
+			this.userTags = new TagCollection;
+			this.userTags.url = "/users/"+currentUserLogin+"/repos/"+ this.get("full_name") +"/tags";
+
+			this.bind("change", function(){
+				this.tags.setUrl(this);
+				this.tags.fetch();
+				
+				this.userTags.setUrl(this);
+				this.userTags.fetch();
+			})
+		}
+		
+	})
+	
+  var Tag = Backbone.Model.extend({
+    defaults: {
+      name: 'a tag',
+			relative_count : "a",
+      total: '=)'
+    }
+  });
+	
+// COLLECTIONS	
+	var TagCollection = Backbone.Collection.extend({
+		model : Tag,
+		initialize : function(){
+			console.log("tagcollection initialized");
+		},
+
+		setUrl : function(repo){
+			this.url = "/repos/"+repo.get("full_name")+"/tags";
+		}
+		
+	});
+	
+// VIEWS	
+	RepoView = Backbone.View.extend({
+		model : Repo,
+		
+		initialize : function(){
+			
+			this.model.bind("change", function(){
+				$("#tag_panel_container")
+					.find("a.repo_name")
+					.text(this.get("full_name"))
+					.find("input.full_name")
+					.val(this.get("full_name"));
+			})
+		}
+	});
+	
+	var TagView = Backbone.View.extend({
+		el : "li",
+		communityTmpl : $("#tagTemplateAdd").html(),
+		personalTmpl : $("#tagTemplateRemove").html(),
+
+		initialize : function(){
+			console.log("new tagview");
+		},
+		
+		renderCommunity : function(){
+			return $.mustache(this.communityTmpl, this.model.attributes);
+		},
+		
+		renderPersonal: function(){
+			return $.mustache(this.personalTmpl, this.model.attributes);
+    }
+	});
+
+
+	var TagCollectionView = Backbone.View.extend({
+		el: '#add_tag_container', 
+		initialize : function(){
+			this.collection.bind("reset", this.render)
+		},
+		render : function(){
+			console.log("re-render tag collection");
+			var cache = "";
+			_(this.models).each(function(tag){
+				cache += new TagView({model : tag}).renderCommunity();
+			})
+			
+			$("#add_tag_container")
+				.html(cache)
+				.find("li").hover(function(){
+						$(this).find("span.options").show();
+					}, function(){
+						$(this).find("span.options").hide();
+					}
+				);
+				
+		},
+	})
+
+
+	
+	// set the common tag panel repo and tagpanelrepo tags collection variables and just keep rewriting them.
+	tagPanelRepo = new Repo();
+	// ok show the repo View which is the tagPanel (one view);
+	var repoView = new RepoView({model : tagPanelRepo});
+	var tagsView = new TagCollectionView({collection :tagPanelRepo.tags});
+
+
+	/* open tag panel to add a tag */
+	$("a.add_tag").click(function(e){
+		var full_name = $(this).attr("title");
+
+		// event listeniers on "change" 
+		tagPanelRepo.set({"full_name" : full_name});
+
+		$("#filters_container").hide();
+		$("#tag_panel_container").slideDown("fast")
+		
+		/*
+		if (currentUserLogin !== ""){
+			$.ajax({
+				dataType: "json",
+			  url: "/users/"+currentUserLogin+"/repos/"+ full_name +"/tags",
+			  success: function ( data ) {
+					var $container = $("#my_tags_on_repo").empty();
+					$("#tagTemplateRemove").tmpl(data).appendTo($container);
+					$container.find("li").hover(function(){
+							$(this).find("span.options").show();
+						}, function(){
+							$(this).find("span.options").hide();
+						}
+					)
+				}
+			});
+		}
+		*/	
+		e.preventDefault();
+		return false;
+	});
+
 
 	$("#tag_panel").submit(function(e){
 		var $form = $(this);
@@ -16,34 +164,6 @@ $(function(){
 		e.preventDefault();
 		return false;
 	})
-	
-	/* open tag panel to add a tag */
-	$("a.add_tag").click(function(e){
-		var full_name = $(this).attr("rel");
-
-		$("#filters_container").hide();
-		$("#tag_panel_container").slideDown("fast")
-			.find("a.repo_name").text($(this).attr("title"));
-		$("#tag_panel_container")
-			.find("input.full_name").val(full_name);
-		
-		if (currentUserLogin !== ""){
-			$.ajax({
-				dataType: "json",
-			  url: "/users/"+currentUserLogin+"/repos/"+ full_name +"/tags",
-			  success: addMyTags
-			});
-			$.ajax({
-			  dataType: "json",
-			  url: "/repos/"+ full_name +"/tags",
-			  success: addTopTags
-			});
-		}
-			
-		e.preventDefault();
-		return false;
-	});
-
 
 	$("a.tag_panel_close").click(function(e){
 		$("#tag_panel_container").hide();
@@ -53,7 +173,7 @@ $(function(){
 	})
 	
 	/* Remove tag from a repo */
-	$("#my_tags_on_repo").find("a").live("click", function(e){
+	$("#my_tags_on_repo").find("span.options").find("a.danger").live("click", function(e){
 		var $tag = $(this);
 		var tag = $tag.attr("title");
 		if(confirm("Remove tag:'" +tag+ "' from this repo?")){
@@ -64,7 +184,6 @@ $(function(){
 			    url: "/untag?repo[full_name]="+full_name+"&tag="+tag+"",
 			    success: function(rsp){
 						showStatus.respond(rsp);
-						$tag.remove();
 					}
 			});
 		}
@@ -80,6 +199,7 @@ $(function(){
 		return false;
 	})
 	
+	
 	function formatTags(){
 		var activeTags = [];
 		
@@ -90,13 +210,4 @@ $(function(){
 		$("#tagging_input").val(activeTags.join(":"));
 	}
 
-	function addTopTags( data ) {
-	  $("#tagTemplate").tmpl(data).appendTo($("#add_tag_container").empty());
-	}
-	
-	function addMyTags( data ) {
-		
-	  $("#tagTemplate").tmpl(data).appendTo($("#my_tags_on_repo").empty());
-	}
-	
 })
