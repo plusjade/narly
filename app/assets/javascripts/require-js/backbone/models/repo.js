b10c56a7190196bb3d277c89b4ce314e54cef201
Jade Dominguez <plusjade@gmail.com>
2011-11-19 02:24:53 -0600

    The tag data should come in as a nested array on the repo

--- app/assets/javascripts/require-js/backbone/models/repo.js
+++ app/assets/javascripts/require-js/backbone/models/repo.js
@@ -8,19 +8,14 @@ define([
 ], function($, _, Backbone, z, RepTagCollection, UserRepoTagCollection){
 	Repo = Backbone.Model.extend({
 		initialize : function(){
-			this.tags = new RepoTagCollection;
+			this.tags = new RepoTagCollection(this.get("tags"));
 			this.tags.setRepo(this);
-			
 			this.userTags = new UserRepoTagCollection;
 			this.userTags.setRepo(this);
 
 			this.bind("change", function(){
 				this.tags.setRepo(this);
 			})
-		},
-		setUser : function(user){
-			this.user = user;
-			this.userTags.setUser(user);
 		},
 		refresh : function(){
 			this.tags.fetch();
