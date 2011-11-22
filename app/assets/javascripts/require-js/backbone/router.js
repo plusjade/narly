2d5031ba4567b0a3406826c923d157baefe542ec
Jade Dominguez <plusjade@gmail.com>
2011-11-21 18:06:29 -0600

    Use a splat on the tags to get everything and remove the unused functions here

--- app/assets/javascripts/require-js/backbone/router.js
+++ app/assets/javascripts/require-js/backbone/router.js
@@ -7,18 +7,10 @@ define([
 	return Backbone.Router.extend({
 
 	  routes: {
-	   	"users/:login/repos/tagged/:tags": "users_repos_tagged",
+	   	"users/:login/repos/tagged/*tags": "users_repos_tagged",
 	 	  "users/:login": "users",
-	  },
-	
-		users_repos_tagged : function(){
-			console.log("users_repos_tagged route");
-		},
-		
-		users : function(){
-			console.log("users route");
-		}
-
+	  }
+			
 	});
 
 });
